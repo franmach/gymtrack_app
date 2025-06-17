c7c79889cb6833ec374c13eb517b76c8a00bd197
franmach <franmach20@outlook.com>
2025-06-17 20:53:30 -0300

    implementacion de basa de datos basica

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Este archivo se genera automáticamente
 
-void main() {
+void main() async {  
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const GymTrackApp());
 }
 
